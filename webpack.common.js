4aaf15fa36a8fc233fcbaed87c421f60d9f1363b
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-04-14 16:16:42 +0700

    use dotenv-webpack package
    - use dotenv-webpack
    - add dotenv-webpack configuration in webpack.common.js
    - add .env.example file

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 const path = require('path');
 
 module.exports = {
@@ -36,5 +37,6 @@ module.exports = {
         },
       ],
     }),
+    new Dotenv(),
   ],
 };
