268c26cd92904ed038c6991c474b13f8b025e9a7
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-04-22 09:55:25 +0700

    init notification

--- src/scripts/utils/web-socket-initiator.js
+++ src/scripts/utils/web-socket-initiator.js
@@ -1,3 +1,6 @@
+import THEMOVIEDB_API_CONFIG from '../globals/themoviedb-api-config';
+import NotificationHelper from './notification-helper';
+
 const WebSocketInitiator = {
   init(url) {
     const webSocket = new WebSocket(url);
@@ -5,7 +8,14 @@ const WebSocketInitiator = {
   },
 
   _onMessageHandler(message) {
-    console.log(message.data);
+    const movie = JSON.parse(message.data);
+    NotificationHelper.sendNotification({
+      title: `${movie.title} is now available in the cinema`,
+      options: {
+        body: movie.overview,
+        image: THEMOVIEDB_API_CONFIG.BASE_IMAGE_URL + movie.poster_path,
+      },
+    });
   },
 };
 
