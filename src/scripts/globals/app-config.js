127d9010ed4c5ecb126ad5970302a76e81f3c46c
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-04-22 09:32:59 +0700

    init web socket

--- src/scripts/globals/app-config.js
+++ src/scripts/globals/app-config.js
@@ -2,6 +2,7 @@ const APP_CONFIG = {
   DATABASE_NAME: 'movie-catalogue-db',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'movies',
+  WEB_SOCKET_URL: 'wss://movies-feed.dicoding.dev',
 };
 
 export default APP_CONFIG;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,8 @@ import '../styles/style.css';
 import '../styles/responsive.css';
 import App from './views/app';
 import serviceWorkerRegister from './utils/service-worker-register';
+import WebSocketInitiator from './utils/web-socket-initiator';
+import APP_CONFIG from './globals/app-config';
 
 const app = new App({
   button: document.querySelector('#hamburgerButton'),
@@ -17,4 +19,5 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', () => {
   app.renderPage();
   serviceWorkerRegister();
+  WebSocketInitiator.init(APP_CONFIG.WEB_SOCKET_URL);
 });
